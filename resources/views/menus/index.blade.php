7117a0814b67ab2d42f0a9c6083d3833fb16556b
destarangga <muhamaddestarangga@smkwikrama.sch.id>
2024-10-21 09:06:39 +0700

    first commit

--- resources/views/menus/index.blade.php
+++ resources/views/menus/index.blade.php
@@ -3,48 +3,39 @@
 @section('content')
 <div class="container">
     <div class="d-flex justify-content-between mt-3 mb-3">
-    <h2>Daftar Menu</h2>
-    <a href="{{ route('menus.create') }}" class="btn btn-primary">Tambah Menu</a>
+        <h2>Daftar Menu</h2>
+        <a href="{{ route('menus.create') }}" class="btn btn-primary">Tambah Menu</a>
     </div>
-    
+
     @if(session('success'))
         <div class="alert alert-success">{{ session('success') }}</div>
     @endif
 
-    <table class="table">
-        <thead>
-            <tr>
-                <th>Nama Menu</th>
-                <th>Deskripsi</th>
-                <th>Harga</th>
-                <th>Gambar</th>
-                <th>Aksi</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach($menus as $menu)
-                <tr>
-                    <td>{{ $menu->name }}</td>
-                    <td>{{ $menu->description }}</td>
-                    <td>Rp{{ number_format($menu->price, 2, ',', '.') }}</td>
-                    <td>
-                        @if($menu->image_path)
-                            <img src="{{ url('image/menu/' . $menu->image_path) }}" alt="Menu Image" style="max-width: 100px;">
-                        @else
-                            <span>Tidak ada gambar</span>
-                        @endif
-                    </td>
-                    <td>
+    <div class="row">
+        @foreach($menus as $menu)
+        <div class="col-md-4 mb-4">
+            <div class="card">
+                @if($menu->image_path)
+                <img src="{{ url('image/menu/' . $menu->image_path) }}" class="card-img-top" alt="Menu Image" style="height: 200px; object-fit: cover;">
+                @else
+                <img src="{{ asset('default-placeholder.png') }}" class="card-img-top" alt="No Image" style="height: 200px; object-fit: cover;">
+                @endif
+                <div class="card-body">
+                    <h5 class="card-title">{{ $menu->name }}</h5>
+                    <p class="card-text">{{ $menu->description }}</p>
+                    <p class="card-text">Harga: Rp{{ number_format($menu->price, 2, ',', '.') }}</p>
+                    <div class="d-flex justify-content-between">
                         <a href="{{ route('menus.edit', $menu) }}" class="btn btn-warning">Edit</a>
                         <form action="{{ route('menus.destroy', $menu) }}" method="POST" style="display:inline;">
                             @csrf
                             @method('DELETE')
                             <button type="submit" class="btn btn-danger">Hapus</button>
                         </form>
-                    </td>
-                </tr>
-            @endforeach
-        </tbody>
-    </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+        @endforeach
+    </div>
 </div>
 @endsection
